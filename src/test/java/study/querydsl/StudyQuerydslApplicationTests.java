86bfbebf3edff56bb9988c214f435c33ad1a4781
jade <jade@insterior.co.kr>
2022-01-06 14:12:42 +0900

    add source

--- src/test/java/study/querydsl/StudyQuerydslApplicationTests.java
+++ src/test/java/study/querydsl/StudyQuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.*;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
+@Transactional
 @SpringBootTest
 class StudyQuerydslApplicationTests {
 
+    @PersistenceContext
+    EntityManager em;
+
     @Test
-    void contextLoads() {
+     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+            .selectFrom(qHello)
+            .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
