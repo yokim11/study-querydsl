1e61bb5fd9cccea3c31e6c7094d3c8d2b6576cd3
jade <jade@insterior.co.kr>
2022-01-06 13:27:13 +0900

    first commit

---
+++ src/main/java/study/querydsl/StudyQuerydslApplication.java
@@ -0,0 +1,13 @@
+package study.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyQuerydslApplication.class, args);
+    }
+
+}
